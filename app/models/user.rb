7dffbca2aac1058e0592a26127245743e4e02259
turquoise <monica.gosschalk@btinternet.com>
2016-03-04 23:58:20 +0000

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-  attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 	validates :name,  presence: true, length: { maximum: 50 }
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, length: { maximum: 255 },
@@ -34,6 +35,30 @@ class User < ActiveRecord::Base
     # Forgets a user.
     def forget
       update_attribute(:remember_digest, nil)
+    end
+
+    # Activates an account.
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 
 end
