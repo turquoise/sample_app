451d9cb579acbce60177b957470045e21433a7d1
turquoise <monica.gosschalk@btinternet.com>
2016-03-19 11:57:39 +0000

    Added figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'monica-sampleapp.herokuapp.com/'
+  host = 'monica-sampleapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
